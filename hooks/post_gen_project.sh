8f7c731f8937e5595757f12fee72e66993845978
Géraud Le Falher <geraud.le.falher@zalando.de>
2020-04-18 18:34:57 +0200

    improve initial git setup

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,6 +4,7 @@ IFS=$'\n\t'
 
 git init
 git add pyproject.toml README.rst setup.cfg setup.py **/*.py
+git add .gitignore .pre-commit-config.yaml LICENSE Makefile docs/index.rst
 command -v poetry >/dev/null 2>&1 || { echo >&2 "poetry not there so won't do anything further"; exit 0; }
 poetry install --no-root
 poetry run pre-commit install
